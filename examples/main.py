bc5a0f9db0af09bd499b3083a4b7ff771aefd2ee
Jack Homan <homan.jack@gmail.com>
2022-02-11 02:09:14 -0500

    examples

--- examples/main.py
+++ examples/main.py
@@ -1,43 +1,39 @@
-from http import HTTPStatus
-
-from fastapi.exceptions import RequestValidationError
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from intercept import mutating, defaulting
-from kubernetes.client import V1Pod, V1EnvVar
+from intercept.webhook import Manager, GVK
+from intercept.webhook import subresource
+from intercept.webhook import set_defaults
+from kubernetes.client import V1Pod, V1EnvVar, V1Container
 
 
-app = FastAPI()
+webhook = Manager()
 
 
-@app.post("/mutate-add-env-var")
-@defaulting.pod
-def add_env_var(pod: V1Pod):
-    pod.spec.containers[0].env = []
+@webhook.defaulting.pod(labels=dict(foo="bar"))
+@subresource("spec", "containers", 0)
+@set_defaults(env=list)
+def add_env_var_1(container: V1Container):
+    container.env.append(V1EnvVar(name="USER", value="jhoman"))
+
+
+@webhook.defaulting(resource=GVK("", "v1", "Pod"), labels=dict(foo="bar"))
+@subresource("spec", "containers", 0)
+def add_env_var_2(container: V1Container):
+    container.env.append(V1EnvVar(name="TMP", value="/tmp"))
+
+
+# @webhook.defaulting(resource=GVK("", "v1", "ServiceAccount"), labels=dict(foo="bar"))
+# @subresource("metadata", "annotations")
+# def add_annotation(annotations: dict):
+#     from fastapi.logger import logger
+#     logger.warning(annotations)
+#     annotations["key"] = "value"
+
+
+@webhook.defaulting.pod(labels=dict(foo="bar"))
+def add_env_var_3(pod: V1Pod):
     pod.spec.containers[0].env.append(V1EnvVar(
-        name="USER", value="jhoman"
+        name="USERNAME", value="bhoman"
     ))
 
 
-@app.exception_handler(RequestValidationError)
-def validation_error(request: Request, exc: RequestValidationError):
-    from fastapi.logger import logger
-    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
-    logger.error(f"{request}: {exc_str}")
-    content = {'status_code': 10422, 'message': exc_str, 'data': None}
-    return JSONResponse(content=content, status_code=HTTPStatus.UNPROCESSABLE_ENTITY)
-
-
 if __name__ == "__main__":
-    import os
-    from uvicorn import run
-    tmpdir = os.getenv("$TMP", "/tmp")
-    cert_dir = os.path.join(tmpdir, "serving-certs")
-    run(
-        app,
-        host="0.0.0.0",
-        port=8888,
-        log_level="info",
-        ssl_certfile=os.path.join(cert_dir, "tls.crt"),
-        ssl_keyfile=os.path.join(cert_dir, "tls.key"),
-    )
+    webhook.start()
